d65dd53763dbec4a3d92673b441ded209064992b
Sam <contactsamie@gmail.com>
2016-07-13 00:03:42 -0400

    Update

---
+++ 126- utilize akka tdd updates/AkkaDotNetTDD/AkkaDotNetTDD.Tests/when_send_email_actor_is_used.cs
@@ -0,0 +1,135 @@
+﻿using Akka.Actor;
+using Akka.Tdd.AutoFac;
+using Akka.Tdd.Core;
+using Akka.Tdd.TestKit;
+using Autofac;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+using ActorsLib;
+using DisplayServiceLib;
+using EmailServiceLib;
+using FakeItEasy;
+
+namespace AkkaDotNetTDD.Tests
+{
+    [TestClass]
+    public class when_send_email_actor_is_used
+    {
+        //Install-Package Akka.Tdd.AutoFac
+        [TestMethod]
+        public void basic_actor_test()
+        {
+            //Arrange
+            var container = new ContainerBuilder().Build();
+            var system = new ApplicationActorSystem();
+            system.RegisterAndCreateActorSystem(new AutoFacAkkaDependencyResolver(container));
+            var factory = new TddTestKitfactoryFactory(container, system.ActorSystem);
+
+            var messageReceived = false;
+            var emailSenderActor = factory.WhenActorReceives<string>().ItShouldDo(actor =>
+            {
+                messageReceived = true;
+            }).CreateMockActorRef<MockActor>();
+
+            //Act
+            emailSenderActor.Tell("hello");
+
+            //Assert
+            factory.AwaitAssert(() => Assert.IsTrue(messageReceived));
+        }
+        [TestMethod]
+        public void introducing_email_service()
+        {
+            //Arrange
+            var builder = new ContainerBuilder();
+            var emailService = A.Fake<IEmailService>();
+            var emailMessage = new EmailMessage()
+            {
+                ToEmail = "to@email.com",
+                FromEmail = "from@email.com",
+                Subject = "subject",
+                Body = "body"
+            };
+            A.CallTo(() => emailService.SendEmail(emailMessage)).Returns(true);
+            builder.Register(b => emailService);
+            var container = builder.Build();
+            var system = new ApplicationActorSystem();
+            system.RegisterAndCreateActorSystem(new AutoFacAkkaDependencyResolver(container));
+            var factory = new TddTestKitfactoryFactory(container, system.ActorSystem);
+
+            
+            var emailSenderActor = factory.WhenActorReceives<string>().ItShouldDo(actor =>
+            {
+              var actorEmailService=  container.Resolve<IEmailService>();
+                actorEmailService.SendEmail(emailMessage);
+            }).CreateMockActorRef<MockActor>();
+
+            //Act
+            emailSenderActor.Tell("hello");
+
+            //Assert
+            factory.AwaitAssert(() => A.CallTo(() => emailService.SendEmail(emailMessage)).MustHaveHappened());
+        }
+
+
+        [TestMethod]
+        public void creating_concrete_actor()
+        {
+            //Arrange
+            var builder = new ContainerBuilder();
+            var emailService = A.Fake<IEmailService>();
+           
+            var emailMessage = new EmailMessage()
+            {
+                ToEmail = "to@email.com",
+                FromEmail = "from@email.com",
+                Subject = "subject",
+                Body = "body"
+            };
+            A.CallTo(() => emailService.SendEmail(emailMessage)).Returns(true);
+
+            //*****TEST FIXES
+            var displayService = A.Fake<IDisplayService>();
+            A.CallTo(() => displayService.SendDisplayMessage(null)).Returns(true);
+            builder.Register(b => displayService);
+            //*****TEST FIXES - END
+
+            builder.Register(b => emailService);
+            var container = builder.Build();
+            var system = new ApplicationActorSystem();
+            system.RegisterAndCreateActorSystem(new AutoFacAkkaDependencyResolver(container));
+            var factory = new TddTestKitfactoryFactory(container, system.ActorSystem);
+
+            //Act
+            var emailSenderActor = system.ActorSystem.CreateActor<EmailSenderActor>();
+            emailSenderActor.Tell("hello");
+
+            //Assert
+            factory.AwaitAssert(() => A.CallTo(() => emailService.SendEmail(emailMessage)).WithAnyArguments().MustHaveHappened());
+        }
+
+        //using new api from testkit - getting message received inside inline actor
+        [TestMethod]
+        public void basic_actor_test2()
+        {
+            //Arrange
+            const string messageString = "hello";
+            var container = new ContainerBuilder().Build();
+            var system = new ApplicationActorSystem();
+            system.RegisterAndCreateActorSystem(new AutoFacAkkaDependencyResolver(container));
+            var factory = new TddTestKitfactoryFactory(container, system.ActorSystem);
+
+            var messageReceived = "";
+            var emailSenderActor = factory.WhenActorReceives<string>().ItShouldDo(actor =>
+            {
+                messageReceived = actor.MessageReceived.ToString();
+            }).CreateMockActorRef<MockActor>();
+
+            //Act
+            emailSenderActor.Tell(messageString);
+
+            //Assert
+            factory.AwaitAssert(() => Assert.AreEqual(messageString,messageReceived));
+        }
+    }
+}
---
+++ 126- utilize akka tdd updates/AkkaDotNetTDD/EmailSenderCoOrdinatorActorApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ActorsLib;
+using Akka.Actor;
+using Akka.Tdd.AutoFac;
+using Akka.Tdd.Core;
+using Autofac;
+using DisplayServiceLib;
+using EmailServiceLib;
+
+namespace EmailSenderCoOrdinatorActorApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting EmailSenderCoOrdinatorActor App ...");
+            var builder = new ContainerBuilder();
+            builder.Register<IEmailService>(b => new EmailService());
+            builder.Register<IDisplayService>(b => new ConsoleDisplayService());
+            var container = builder.Build();
+            var system = new ApplicationActorSystem();
+            system.RegisterAndCreateActorSystem(new AutoFacAkkaDependencyResolver(container));
+            var emailSenderActorCoOrdinator = system.ActorSystem.CreateActor<EmailSenderActorCoOrdinator<EmailSenderActor>>();
+            while (true)
+            {
+                var message = Console.ReadLine();
+                emailSenderActorCoOrdinator.Tell("hello");
+            }
+        }
+    }
+}
